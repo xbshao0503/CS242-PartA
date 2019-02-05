e86262e736580fca374c0b360d4447f1b373b386
Xinbo Shao <xinboshao_96@outlook.com>
2019-02-06 03:59:20 +0800

    Initial commit

---
+++ src/main/java/CreateLuceneIndex.java
@@ -0,0 +1,101 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.StoredField;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.index.IndexWriterConfig.OpenMode;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.util.Version;
+
+public class CreateLuceneIndex {
+
+
+    public CreateLuceneIndex(String tweetFile, String luceneDir)
+            throws IOException {
+
+        System.out.println("Indexing to directory '" + luceneDir + "'...");
+//        File inutfile = new File(luceneDir);
+        String outputpath = luceneDir;
+
+        Directory dir = FSDirectory.open(Paths.get(outputpath));
+        Analyzer analyzer = new StandardAnalyzer();
+        IndexWriterConfig iwc = new IndexWriterConfig(analyzer);
+
+        // Create will overwrite the index everytime
+        iwc.setOpenMode(OpenMode.CREATE);
+
+        // Create an index writer
+        IndexWriter writer = new IndexWriter(dir, iwc);
+
+        // Add files to index
+        indexTweets(writer, tweetFile);
+        System.out.println("done");
+
+        // This makes write slower but search faster.
+        writer.forceMerge(1);
+
+        writer.close();
+
+    }
+
+    private void indexTweets(IndexWriter writer, String tweetFile) throws IOException {
+        System.out.println("Reading File");
+        String line = null;
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new FileReader(tweetFile));
+            line = br.readLine();
+            int count = 0;
+            while (line != null) {
+                // extract UserId
+                String word = line.substring(0,line.indexOf('\t'));
+                line = line.substring(line.indexOf('\t')+1);
+                System.out.println(line);
+
+                    String[] pieces = line.split("\\:");
+
+                Document doc = new Document();
+                doc.add(new TextField("text", word, Field.Store.YES));
+                doc.add(new StoredField("id", pieces[0]));
+                doc.add(new StoredField("score", pieces[1]));
+
+                writer.addDocument(doc);
+                System.out.println(doc);
+                line = br.readLine();
+                count ++;
+                if (count % 100 == 0){
+                    System.out.println("100 words");
+                    count = 0;
+                }
+            }
+        } catch (IOException e) {
+            System.err.println("Failed to read userFile: " + e.getMessage());
+        }
+
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        if (args.length < 2) {
+            System.out.println("Usage [userFile] [luceneDir]");
+            System.exit(-1);
+        }
+        long startTime = System.currentTimeMillis();
+        CreateLuceneIndex luceneCreator = new CreateLuceneIndex(args[0],
+                args[1]);
+        long endTime = System.currentTimeMillis();
+        System.out.println("Total execution time:" + (endTime-startTime));
+    }
+}
---
+++ src/main/java/test.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+public class test {
+    public static void main(String[] args) throws IOException {
+        String input = "C:\\Users\\Xinbo Shao\\Desktop\\lucenecore\\src\\main\\resources\\data1.json";
+
+
+
+
+
+    }
+
+}
