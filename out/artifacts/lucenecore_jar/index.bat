f810bf74865841d7f1dc1272713ed8ed1998bba8
Xinbo Shao <xinboshao_96@outlook.com>
2019-07-01 16:56:49 +0800

    Final version

---
+++ out/artifacts/lucenecore_jar/index.bat
@@ -0,0 +1,3 @@
+@echo off
+java -jar lucenecore.jar "C:\\Users\\Xinbo Shao\\Desktop\\lucenecore\\src\\main\\resources\\5k.json" "C:\Users\Xinbo Shao\Desktop\lucenecore\src\main\resources" "C:\Users\Xinbo Shao\Desktop\lucenecore\src\main\resources1" "valentine"
+pause
--- src/main/java/CreateLuceneIndex.java
+++ src/main/java/CreateLuceneIndex.java
@@ -1,12 +1,12 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
 
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.core.KeywordAnalyzer;
+import org.apache.lucene.analysis.core.StopAnalyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
@@ -17,56 +17,140 @@ import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.index.IndexWriterConfig.OpenMode;
 import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.queryparser.flexible.core.QueryNodeException;
+import org.apache.lucene.queryparser.flexible.standard.StandardQueryParser;
 import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.ScoreDoc;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
-import org.apache.lucene.util.Version;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+import org.apache.lucene.search.similarities.BM25Similarity;
+import static java.lang.Math.min;
 
 public class CreateLuceneIndex {
 
-
-    public CreateLuceneIndex(String Data_File, String Index_Dir)
+    // some function are annotated, they can still work. Please check it!
+    public CreateLuceneIndex(String Data_File, String Index_Dir, String Index_Dir1, String query_text)
             throws IOException, org.apache.lucene.queryparser.classic.ParseException {
 
         System.out.println("Indexing beginning");
         Directory dir = FSDirectory.open(Paths.get(Index_Dir));
-        Analyzer analyzer = new StandardAnalyzer();
+        System.out.println(dir);
+        //Directory dir1 = FSDirectory.open(Paths.get(Index_Dir1));
+        Analyzer analyzer = new StopAnalyzer();
         IndexWriterConfig config = new IndexWriterConfig(analyzer);
+
+
+        //For tweets contents, removing stop words
+        Analyzer analyzer1 = new StopAnalyzer();
+        //IndexWriterConfig config1 = new IndexWriterConfig(analyzer1);
 
         // set write mode
         config.setOpenMode(OpenMode.CREATE);
+        //config1.setOpenMode(OpenMode.CREATE);
         IndexWriter writer = new IndexWriter(dir, config);
+        //IndexWriter writer1 = new IndexWriter(dir1, config1);
 
         // Add files to index
         Indexing_Data_From_Twitter(writer, Data_File);
+
+        //Indexing_Tweets_From_Twitter(writer1, Data_File);
+
+
+
         System.out.println("Indexing finished");
 
         writer.close();
         System.out.println("=====================================");
         System.out.println("=====================================");
 
-        System.out.println("Query Searching beginning");
-        DirectoryReader indexReader = DirectoryReader.open(dir);
-        IndexSearcher indexSearcher = new IndexSearcher(indexReader);
-        QueryParser parser = new QueryParser("text", analyzer);
-        Query query = parser.parse("Ali Baba");
+        Using_query_search(dir, analyzer, query_text,Index_Dir);
 
-        System.out.println(query.toString());
-        int topHitCount = 100;
-        ScoreDoc[] hits = indexSearcher.search(query, topHitCount).scoreDocs;
+    }
 
-        for (int rank = 0; rank < hits.length; ++rank) {
-            Document hitDoc = indexSearcher.doc(hits[rank].doc);
-            System.out.println((rank + 1) + " (score:" + hits[rank].score + ") --> " + hitDoc.get("text"));
+    private void Using_query_search(Directory dir,Analyzer analyzer, String query_text, String Index_Dir ){
+        //        Seaching query
+        try {
+            System.out.println("Query Searching beginning");
+            DirectoryReader indexReader = DirectoryReader.open(dir);
+            IndexSearcher indexSearcher = new IndexSearcher(indexReader);
+            QueryParser parser = new QueryParser("textandhashTag", analyzer);
+
+            // attention: query need to parse
+            Query query = parser.parse(query_text);
+
+
+            System.out.println(query.toString());
+            int topHitCount = 100;
+            ScoreDoc[] hits = indexSearcher.search(query, topHitCount).scoreDocs;
+
+            JsonArray result = new JsonArray();
+            for (int rank = 0; rank < hits.length; ++rank) {
+                Document hitDoc = indexSearcher.doc(hits[rank].doc);
+                System.out.println((rank + 1) + " (score:" + hits[rank].score + ") --> " + "tweet_content:" + hitDoc.get("text") + " --> " +" hashtag:" + hitDoc.get("hashTag") +" user_id:" + " --> " +  hitDoc.get("id") + " --> " +" time:"+  hitDoc.get("timestamp") +" geo-location:"+  hitDoc.get("geo_location"));
+                JsonObject tweet_info = new JsonObject();
+                tweet_info.addProperty("rank",rank+1);
+                tweet_info.addProperty("text",hitDoc.get("text"));
+                tweet_info.addProperty("hashtag",hitDoc.get("hashTag"));
+                tweet_info.addProperty("user_id",hitDoc.get("id"));
+                tweet_info.addProperty("timestamp",hitDoc.get("timestamp"));;
+                tweet_info.addProperty("geo_location",hitDoc.get("geo_location"));
+                result.add(tweet_info);
+
+            }
+            System.out.println(result.toString());
+            System.out.println("Query Searching finished");
+            String json_dir = Index_Dir+"\\data1.json";
+            System.out.println(json_dir);
+            File result_file = new File(json_dir);
+            if (result_file.exists()) { // 如果已存在,删除旧文件
+                result_file.delete();
+            }
+            Writer write = new OutputStreamWriter(new FileOutputStream(result_file), "UTF-8");
+            write.write(result.toString());
+            write.flush();
+            write.close();
+
+        }catch (IOException e) {
+            e.printStackTrace();
+        } catch (org.apache.lucene.queryparser.classic.ParseException e) {
+            e.printStackTrace();
         }
 
-        System.out.println("Query Searching finished");
+
+    }
+
+
+    private void Indexing_Tweets_From_Twitter(IndexWriter writer, String Data_File) throws IOException {
+        System.out.println("Parsering");
+        JSONParser parser = new JSONParser();
+        try {
+            Object obj = parser.parse(new FileReader(Data_File));
+            JSONArray jsonArray = (JSONArray) obj;
+            for (int i=0;i<jsonArray.size();i++){
+                JSONObject jsonObject = (JSONObject) jsonArray.get(i);
+                //get twitter contents
+                String text = (String) jsonObject.get("text");
+
+                Document doc = new Document();
+                doc.add(new TextField("text", text, Field.Store.YES));
+
+                writer.addDocument(doc);
+
+            }
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+
 
 
 
@@ -75,8 +159,10 @@ public class CreateLuceneIndex {
     private void Indexing_Data_From_Twitter(IndexWriter writer, String Data_File) throws IOException {
         System.out.println("Parsering");
         JSONParser parser = new JSONParser();
+        BufferedReader br = new BufferedReader(new FileReader(Data_File),1024);
+
         try {
-            Object obj = parser.parse(new FileReader(Data_File));
+            Object obj = parser.parse(br);
             JSONArray jsonArray = (JSONArray) obj;
             for (int i=0;i<jsonArray.size();i++){
                 JSONObject jsonObject = (JSONObject) jsonArray.get(i);
@@ -100,8 +186,11 @@ public class CreateLuceneIndex {
 
                 //get twitter url
                 JSONArray urls = (JSONArray) entities.get("urls");
-                JSONObject url_info = (JSONObject) urls.get(0);
-                String url = (String) url_info.get("url");
+                String url = "null";
+                if (urls.size() !=0) {
+                    JSONObject url_info = (JSONObject) urls.get(0);
+                    url = (String) url_info.get("url");
+                }
 
                 //get geolocation information
                 JSONObject coordinates = (JSONObject) jsonObject.get("coordinates");
@@ -114,18 +203,20 @@ public class CreateLuceneIndex {
 
                 Document doc = new Document();
                 doc.add(new TextField("text", text, Field.Store.YES));
-                doc.add(new StoredField("id", userId));
-                doc.add(new StoredField("hashTag", hash_text));
-                doc.add(new StoredField("url", url));
-                doc.add(new StoredField("geo_location", geo_location));
-                doc.add(new StoredField("timestamp", timestamp));
-
+                doc.add(new TextField("id", userId.toString(), Field.Store.YES));
+                doc.add(new TextField("hashTag", hash_text, Field.Store.YES));
+                doc.add(new TextField("url", url, Field.Store.YES));
+                doc.add(new TextField("geo_location", geo_location, Field.Store.YES));
+                doc.add(new TextField("timestamp", timestamp, Field.Store.YES));
+//                doc.add(new StoredField("id", userId));
+//                doc.add(new StoredField("hashTag", hash_text));
+//                doc.add(new StoredField("url", url));
+//                doc.add(new StoredField("geo_location", geo_location));
+//                doc.add(new StoredField("timestamp", timestamp));
 
                 writer.addDocument(doc);
 
-//                System.out.println(doc);
             }
-
 
 
         } catch (IOException e) {
@@ -142,12 +233,12 @@ public class CreateLuceneIndex {
 
 
     public static void main(String[] args) throws IOException, org.apache.lucene.queryparser.classic.ParseException {
-        if (args.length < 2) {
-            System.out.println("Please input args[0]:data.json, args[1]:output_directory");
+        if (args.length < 4) {
+            System.out.println("Please input args[0]:data.json, args[1]:output_directory_for_full_text, args[2]:output_directory_for_only_tweets_contents ");
             System.exit(-1);
         }
         long startTime = System.currentTimeMillis();
-        CreateLuceneIndex Lucene_generator = new CreateLuceneIndex(args[0], args[1]);
+        CreateLuceneIndex Lucene_generator = new CreateLuceneIndex(args[0], args[1], args[2], args[3]);
         long endTime = System.currentTimeMillis();
         System.out.println("Execution time:" + (endTime-startTime));
     }
